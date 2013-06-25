6d59d55422aee451ca6750f67d273da3a57eb4a0
Roman Radenko <roman.radenko@gmail.com>
2013-06-25 19:18:19 -0400

    Commit last changes

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,9 @@ module HelloWorld
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
+    # Heroku
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = false
 
